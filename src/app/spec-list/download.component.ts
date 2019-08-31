d33c8cba523653dbfb84482fb659cee699ac8f8a
Alexander Demeshko <demeshko.alexander.v@gmail.com>
2019-08-31 16:55:02 +0300

    feat(generator): convert state

--- src/app/spec-list/download.component.ts
+++ src/app/spec-list/download.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { State } from '@ngrx/store';
+import { select, State, Store } from '@ngrx/store';
+import { take } from 'rxjs/operators';
 
 import { AppState } from 'src/app/state/root-reducer';
 import { DownloadService } from '../@generator/download.service';
-import { TfAssertType, TfCommand, TfCommandType, TfEventType } from '../@generator/generator-models';
+import { TfAssertType, TfCommand, TfCommandType, TfEventType, TfProject } from '../@generator/generator-models';
 import { GeneratorService } from '../@generator/generator.service';
+import { getConvertedState } from '../state/state.selector';
 
 @Component({
   selector: 'tf-download-button',
@@ -17,40 +19,18 @@ import { GeneratorService } from '../@generator/generator.service';
 })
 export class DownloadComponent {
 
-  constructor(private state: State<AppState>, private generatorService: GeneratorService, private downloadService: DownloadService) {
+  constructor(private store: Store<AppState>, private generatorService: GeneratorService, private downloadService: DownloadService) {
   }
 
   download() {
 
-    const project = {
-      name: 'app',
-      url: 'app',
-      specList: [
-        {
-          name: 'Add Item test',
-          testList: [
-            {
-              name: 'should add item',
-              commandList: [
-                {
-                  target: '/html/body/ec-root/nb-layout/div[1]/div/div/div/div/nb-layout-column/d' +
-                    'iv[2]/ec-colors-panel/div[1]/ec-color-block/ec-color-selector/div/ec-input-color-picker/input',
-                  type: TfCommandType.EVENT,
-                  eventType: TfEventType.CLICK,
-                } as TfCommand,
-                {
-                  target: '//*[@id="cdk-overlay-2"]/nb-popover/nb-overlay-container/ec-color-picker',
-                  type: TfCommandType.ASSERT,
-                  assertType: TfAssertType.EXIST,
-                } as TfCommand,
-              ]
-            },
-          ],
-        },
-      ],
-    };
-
-    this.downloadService.download(this.generatorService.generate(project));
+    this.store.pipe(
+      select(getConvertedState),
+      take(1),
+    )
+      .subscribe((project: TfProject) => {
+        this.downloadService.download(this.generatorService.generate(project));
+      });
   }
 
 }
