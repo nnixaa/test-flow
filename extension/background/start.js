63aadcca3c97d92834b3f39b89a5507811446a08
nixa <4dmitr@gmail.com>
2019-08-31 11:28:29 +0300

    open/close app

--- extension/background/start.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    chrome.tabs.sendMessage(tabs[0].id, { type: "startApp" });
-  });
-});
-
