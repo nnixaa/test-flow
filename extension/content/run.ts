df3b86b39f77940bd84d81881cc3ff60938f276a
nixa <4dmitr@gmail.com>
2019-08-31 17:15:18 +0300

    feat: box shadow

--- extension/content/run.ts
+++ extension/content/run.ts
@@ -10,7 +10,16 @@ import { initializeRecorder } from '../recorder/container';
   const createApp = () => {
     const iframe = document.createElement('iframe');
     iframe.style.cssText = `
-      position:fixed;border: none;top:30px;right:30px;display:block;width:300px;height:560px;z-index:1000000000;
+      position: fixed;
+      border: none;
+      top: 30px;
+      right: 30px;
+      display: block;
+      width: 300px;
+      height: 560px;
+      z-index: 1000000000;
+      border-radius: 0.25rem;
+      box-shadow: 0 0.5rem 1rem 0 rgba(44, 51, 73, 0.1);
     `;
     iframe.src = chrome.extension.getURL('index.html');
     document.body.appendChild(iframe);
